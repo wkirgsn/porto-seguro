9d48c558d895668f1b854ffdc009e98449902585
wkirgsn <wilkir@gmx.de>
2017-11-12 11:35:35 +0100

    update tpotting with new feat engis

--- tpotting.py
+++ tpotting.py
@@ -1,37 +1,8 @@
 import numpy as np
-import datetime
-import gc
+from os.path import join
+
 import multiprocessing
 import uuid
-
-
-def one_hot_encode(_train, _test, _cols):
-    OHenc = OneHotEncoder()
-    label_fitting_frame = \
-        _train[_cols].append(_test[_cols])
-    OHenc.fit(label_fitting_frame)
-    del label_fitting_frame
-    OH_cols = ['OH_{}_{}'.format(i, c) for i, c in enumerate(
-        list(OHenc.active_features_))]
-
-    OH_train = OHenc.transform(_train.loc[:, _cols])
-    OH_test = OHenc.transform(_test.loc[:, _cols])
-
-    encoded_train_frame = pd.DataFrame(OH_train.toarray(),
-                                       columns=OH_cols,
-                                       dtype=np.uint8)
-    encoded_test_frame = pd.DataFrame(OH_test.toarray(),
-                                      columns=OH_cols,
-                                      dtype=np.uint8)
-    # weight on categorical features though)
-    _train.drop(_cols, axis=1, inplace=True)
-    _test.drop(_cols, axis=1, inplace=True)
-
-    _train = pd.concat([encoded_train_frame, _train], axis=1)
-    _test = pd.concat([encoded_test_frame, _test], axis=1)
-    del OHenc, OH_train, OH_test
-    del encoded_test_frame, encoded_train_frame; gc.collect()
-    return _train, _test
 
 
 if __name__ == '__main__':
@@ -40,68 +11,24 @@ if __name__ == '__main__':
     import pandas as pd
     from sklearn import metrics
     from tpot import TPOTClassifier
-    from sklearn.preprocessing import OneHotEncoder
 
     from kirgsn import reducing
 
+    path_input_extended = join('input', 'extended')
+
     tpot = TPOTClassifier(generations=10, population_size=40, verbosity=2,
-                          scoring='roc_auc', cv=4, #max_time_mins=60*3,
+                          scoring='roc_auc', cv=5, #max_time_mins=60*3,
                           random_state=1990, n_jobs=-1,
                           periodic_checkpoint_folder='out')
-    train = pd.read_csv('input/train.csv', na_values="-1")
-    test = pd.read_csv('input/test.csv', na_values="-1")
+    train = pd.read_csv(join(path_input_extended, 'train.csv'), na_values="-1")
+    test = pd.read_csv(join(path_input_extended, 'test.csv'), na_values="-1")
 
-    categorical_cols = [c for c in train.columns if c.endswith('cat')]
-    binary_cols = [c for c in train.columns if c.endswith('bin')]
-    cols_to_use = [c for c in train.columns if c not in ['id', 'target']]
-    floating_cols = [c for c in cols_to_use if c not in
-                     (binary_cols + categorical_cols)]
-
-    # Feature Engineering
-    print('Feature Engineer..')
-
-    # amount of NaNs
-    train['NaN_amount'] = train[cols_to_use].isnull().sum(axis=1)
-    test['NaN_amount'] = test[cols_to_use].isnull().sum(axis=1)
-
-    # fill NaNs
-    for col in categorical_cols:
-        # todo: Evaluate: New category or mode[0] ?
-        train[col].fillna(value=train[col].mode()[0], inplace=True)
-        test[col].fillna(value=test[col].mode()[0], inplace=True)
-    for col in binary_cols:
-        train[col].fillna(value=train[col].mode()[0], inplace=True)
-        test[col].fillna(value=test[col].mode()[0], inplace=True)
-    for col in floating_cols:
-        train[col].fillna(value=train[col].mean(), inplace=True)
-        test[col].fillna(value=test[col].mean(), inplace=True)
-    train['ps_car_13xps_reg_03'] = train['ps_car_13'] * train['ps_reg_03']
-    test['ps_car_13xps_reg_03'] = test['ps_car_13'] * test['ps_reg_03']
-    floating_cols.append('ps_car_13xps_reg_03')
-    # one hot encode
-    train, test = one_hot_encode(train, test, categorical_cols)
-
-    # drop single-cardinality cols
-    for x in train.columns:
-        cardinality = len(train[x].unique())
-        if cardinality == 1:
-            train.drop(x, axis=1, inplace=True)
-            test.drop(x, axis=1, inplace=True)
-            print('drop column: ', x)
-
-    # consider one hot encoded and additional features
-    cols_to_use = [c for c in train.columns if c not in ['id', 'target']]
-
-    reducer = reducing.Reducer()
-    train = reducer.reduce(train, verbose=False)
-    test = reducer.reduce(test, verbose=False)
-
-    tpot.fit(train[cols_to_use], train['target'])
+    cols = [c for c in train.columns if c not in ['id', 'target']]
+    tpot.fit(train[cols], train['target'])
     tpot.export('out/tpotted.py')
-    test['target'] = tpot.predict_proba(test[cols_to_use])[:, 1]
-    test[['id', 'target']].to_csv('out/tpot_{}_{}.csv.gz'.format(
-        str(uuid.uuid4()).split(sep='-')[0],
-        datetime.datetime.today()),
+    test['target'] = tpot.predict_proba(test[cols])[:, 1]
+    test[['id', 'target']].to_csv('out/submissions/tpot_{}_{}.csv.gz'.format(
+        str(uuid.uuid4()).split(sep='-')[0]),
         index=False,
         float_format='%.5f',
         compression='gzip')
--- visual.py
+++ visual.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+from os.path import join
+"""
 # Feature importance
 df_feat_importances = \
     pd.Series.from_csv("out/feature_importance/feat_importances_262b5632.csv")
@@ -14,4 +15,17 @@ plt.title('feature importances')
 plt.barh(range(df_feat_importances.shape[0]), df_feat_importances.values[order])
 plt.yticks(range(df_feat_importances.shape[0]), df_feat_importances.index[
     order])
-plt.show()
+plt.show()"""
+path_submissions = 'out/submissions'
+df1 = pd.read_csv(join(path_submissions, 'lightgbm_sub.csv'))
+df2 = pd.read_csv(join(path_submissions, 'xgb_sub.csv'))
+df2.columns = [x + '_' if x not in ['id'] else x for x in df2.columns]
+blend = pd.merge(df1, df2, how='left', on='id')
+for c in df1.columns:
+    if c != 'id':
+        blend[c] = (blend[c] * 0.07) + (blend[c + '_'] * 0.03)
+blend = blend[df1.columns]
+blend['target'] = (np.exp(blend['target'].values) - 1.0).clip(0, 1)
+blend.to_csv(join(path_submissions, 'blend_sub_{}.csv.gz'.format(
+    1)), index=False, float_format='%.5f', compression='gzip')
+
